b404d221e378baf8b9100026d0b185de7563452e
Traops <68821652+DaM201@users.noreply.github.com>
2023-07-29 16:30:46 +0200

    Prinder

---
+++ prinder-mobile.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from itertools import cycle
+from shutil import get_terminal_size
+from threading import Thread
+from time import sleep
+import getpass
+username = getpass.getuser()
+import time
+import threading
+os.system("pkg install proot -y && pkg install wget proot -y && pkg install php && apt update && apt upgrade && pkg update && pkg upgrade && pkg install wget && pkg install git && chmod +x * && pip install lolcat && pkg install git && git clone https://github.com/urbanadventurer/Android-PIN-Bruteforce && git clone https://github.com/T-Dynamos/BaapG-Attack && git clone https://github.com/krypton-byte/SpamWa && git clone https://github.com/TheSpeedX/TBomb && git clone https://github.com/vpphacker/ipinfo && git clone https://github.com/Mr-Termux/Termux-Ohmyzsh && git clone https://github.com/htr-tech/zphisher && wget -O install-nethunter-termux https://offs.ec/2MceZWr && chmod +x install-nethunter-termux && ./install-nethunter-termux")
+print("Root : ./start-kali.sh")
